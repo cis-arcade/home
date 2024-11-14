5a2e4a7eb49ee57b77139245973e6d75d629894e
supermercado97 <isaiahmercadotx@gmail.com>
2024-11-14 15:43:54 -0600

    added easter eggs from 8th period -- thank you Raymond Samuell

--- minigame0/main.js
+++ minigame0/main.js
@@ -8,12 +8,32 @@ const responses = [
     "Better not tell you now.",
     "Cannot predict now.",
     "Most likely.",
-    "My sources say no."
+    "My sources say no.",
+    "Sorry, my brain isn't working right now.",
   ];
 
   const special = {
     "Am I funny?": "lol no",
     "Is Mr Mercado funny?": "Luck is shining upon you!",
+    "Who am I?" : "Who? A nobody.",
+    "Where am I?": "In your dad's basement eating chips.",
+    "How old am I?": "You're a teen and still don't know how old you are.",
+    "What's your name?": "I have no name but you can call me sir.",
+    "Is  Michael Jordan better than LeBron James" : "in your dreams",
+    "What is a all mexican job site biggest fear " : "are you sure you want to ask this question?",
+    "Why was the math book depressed?" : "Because it had too many problems.",
+    "What does a bee do at the gym?" : "Zumba!",
+    "Why don’t birds use Facebook?" : "Because they already have Twitter.",
+    "What does one iguana say to her twin sister?." : "Iguan-ya, too!",
+    "Why don’t skeletons fight?" : "Because they don’t have the guts.",
+    "Is Mr. Mercado secretly a cat?" : "Absoduckinglutely.",
+    "How many toes do i have." : "Ask ur mom.",
+    "Are you with the nupe?" : "SHE WITH THE NUPES NOW SHE WITH THE NUPES NOW SAID SHE LIKE YOU BUT SHE LOVE ME",
+    "Am I pretty?" : "(Hell nah) or (probably not).",
+    "Man or Bear?" : "Beer.",
+    "Am I gonna be successful?" : "(Nah) or (yup).",
+    "Are you magic?" : "(Hell nah) or (duh)",
+    "Is the sky up?" : "(Erm idk)",
   };
 
   document.getElementById("shake-button").addEventListener("click", function() {
