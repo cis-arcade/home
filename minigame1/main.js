5689ca4eb99c146b53ef068461be93f98a0f1a80
supermercado97 <isaiahmercadotx@gmail.com>
2024-11-22 10:09:36 -0600

    updated charater data

--- minigame1/main.js
+++ minigame1/main.js
@@ -1,34 +1,74 @@
 characterData = [
     {
-        "name" : "Goku",
-        "power" : 0.9,
-        "durability" : 0.9,
-        "speed" : 0.6,
-        "combat skills" : 0.4,
-        "image" : "https://pbs.twimg.com/profile_images/1366466342354751491/JyhZpbtu_400x400.jpg"
+        "name": "Goku",
+        "power": 0.9,
+        "durability": 0.9,
+        "speed": 0.6,
+        "combat-skills": 0.4,
+        "image": "https://m.media-amazon.com/images/I/71q7TSSqO6L._AC_UF894,1000_QL80_.jpg"
     },
     {
-        "name" : "Zoro",
-        "power" : 0.5,
-        "durability" : 0.8,
-        "speed" : 0.3,
-        "combat skills" : 0.8,
-        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZGMBHxkTOfZC0sIeQSgypyHVlLwq8GREORw&s"
+    "name": "Alien Dragon",
+    "power": 0.7,
+    "durability": 0.7,
+    "speed": 0.5,
+    "combat-skills": 0.5,
+    "image": "https://www.gstatic.com/android/keyboard/emojikitchen/20240530/u1f409/u1f409_u1f47d.png?fbx"
     },
     {
-        "name" : "Gumball",
-        "power" : 0.2,
-        "durability" : 0.9,
-        "speed" : 0.9,
-        "combat skills" : 0.4,
-        "image" : "https://static.wikia.nocookie.net/characters/images/1/18/Season_3_Gumball_28Improved_29.png/revision/latest?cb=20230708001747"
+        "name": "Zoro",
+        "power": 0.5,
+        "durability": 0.8,
+        "speed": 0.5,
+        "combat-skills": 0.8,
+        "image": "https://upload.wikimedia.org/wikipedia/en/a/a4/Roronoa_Zoro.jpg"
+    },
+  {
+        "name": "Sasuke Uchiha",
+        "power": 0.8,
+        "durability": 0.7,
+        "speed": 0.85,
+        "combat-skills": 0.9,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAakkY9c-hOlAW-rI3VJye-MdIicv_dobt_Q&s"
+    },
+    {
+        "name": "Sakura Haruno",
+        "power": 0.75,
+        "durability": 0.8,
+        "speed": 0.6,
+        "combat-skills": 0.7,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3lUQRHTtlfiJ8qnsaA2JxbzoVjrwcherPSw&s"
+    },
+    {
+        "name": "Monkey D. Luffy",
+        "power": 0.9,
+        "durability": 0.9,
+        "speed": 0.7,
+        "combat-skills": 0.85,
+        "image": "https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/02/One-Piece-Monkey-D.-Luffy-Cropped-Cropped.jpg"
+    },
+    {
+        "name": "Portgas D. Ace",
+        "power": 0.85,
+        "durability": 0.75,
+        "speed": 0.8,
+        "combat-skills": 0.8,
+        "image": "https://i.scdn.co/image/ab67616d00001e02b6dad05dc0e31656c4110930"
+    },
+    {
+        "name": "Gumball",
+        "power": 0.2,
+        "durability": 0.9,
+        "speed": 0.9,
+        "combat-skills": 0.4,
+        "image": "https://i1.sndcdn.com/avatars-lwOTSVr06nF76Hnk-U3BPQg-t240x240.jpg"
     },
     {
         "name" : "Naruto",
         "power" : 0.9,
         "durability" : 0.7,
         "speed" : 0.9,
-        "combat skills" : 1.2,
+        "combat-skills" : 1.2,
         "image" : "https://variety.com/wp-content/uploads/2024/08/NARSHP_set2_screen_3x2cr-res.jpg?w=1000&h=667&crop=1"
     },
     {
@@ -36,7 +76,7 @@ characterData = [
         "power" : 0.8,
         "durability" : 0.7,
         "speed" : 0.9,
-        "combat skills" : 0.8,
+        "combat-skills" : 0.8,
         "image" : "https://a.storyblok.com/f/178900/960x540/6e6bcda041/demon-slayer.jpg/m/filters:quality(95)format(webp)"
     },
     {
@@ -44,17 +84,16 @@ characterData = [
         "power" : 0.9,
         "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/05/Meliodas-Seven-Deadly-Sins-Cropped.jpg"
 
-  
-  },
+    },
     {
   "name" : "Vanellope",
         "power" : 0.8,
         "durability" : 0.3,
         "speed" : 0.9,
-        "combat skills" : 0.4,
+        "combat-skills" : 0.4,
         "image" : "https://eartotheretravel.com/wp-content/uploads/2015/10/Vanellope_profile.png"
     },
     {
@@ -62,7 +101,7 @@ characterData = [
         "power" : 0.8,
         "durability" : 0.9,
         "speed" : 0.7,
-        "combat skills" : 0.7,
+        "combat-skills" : 0.7,
         "image" : "https://i.ytimg.com/vi/xG_8cepY6Sw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBUZc5LJR445FH-RuFKfM0CjI2Iqw"
      
      },
@@ -71,7 +110,7 @@ characterData = [
         "power" : 0.8,
         "durability" : 0.7,
         "speed" : 0.5,
-        "combat skills" : 0.8,
+        "combat-skills" : 0.8,
         "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpKWbQxztg3izIxyOoczWiTb_pifNeuK8pfA&s"
     
        },
@@ -80,17 +119,17 @@ characterData = [
         "power" : 0.4,
         "durability" : 0.3,
         "speed" : 0.7,
-        "combat skills" : 0.8,
+        "combat-skills" : 0.8,
         "image" : "https://cn.i.cdn.ti-platform.com/content/1539/craig-of-the-creek/showpage/za/craig.48956a2a.jpg"
         
         },
     {
-  "name" : "couragecowardlydog",
+  "name" : "Courage D. Cowardly Dog",
         "power" : 0.1,
         "durability" : 0.5,
         "speed" : 0.9,
-        "combat skills" : 0.4,
-        "image" : "https://www.awn.com/sites/default/files/styles/original/public/image/attached/1058366-couragecowardlydogcsr-1280.jpg?itok=U3ueovvl"
+        "combat-skills" : 0.4,
+        "image" : "https://i.ytimg.com/vi/xjw4Xt4B9yI/hqdefault.jpg"
        
      },
     {
@@ -98,7 +137,7 @@ characterData = [
         "power" : 0.8,
         "durability" : 0.9,
         "speed" : 0.3,
-        "combat skills" : 0.5,
+        "combat-skills" : 0.5,
         "image" : "https://www.famousbirthdays.com/group_images/medium/timmy-turner-played.jpg"
     
      },
@@ -107,7 +146,7 @@ characterData = [
         "power" : 0.5,
         "durability" : 0.9,
         "speed" : 0.6,
-        "combat skills" : 0.7,
+        "combat-skills" : 0.7,
         "image" : "https://content.presspage.com/uploads/1441/1920_commercial-desk.png?10000"
     
      },
@@ -116,8 +155,8 @@ characterData = [
         "power" : 0.3,
         "durability" : 0.9,
         "speed" : 0.9,
-        "combat skills" : 0.7,
-        "image" : "https://d23.com/app/uploads/2018/12/1180w-600h_121218_tinker-bell-fun-facts-780x440.jpg"
+        "combat-skills" : 0.7,
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV4p-BlQ3UzSMU3EkZ1bGiK41vDOvrpH-3TA&s"
     
      },
     {
@@ -125,7 +164,7 @@ characterData = [
         "power" : 0.5,
         "durability" : 0.9,
         "speed" : 0.9,
-        "combat skills" : 0.4,
+        "combat-skills" : 0.4,
         "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKq43gadLEZsjcZohfVzd-zepYpbCiTscJLg&s"
     
     },
@@ -134,15 +173,15 @@ characterData = [
         "power" : 0.6,
         "durability" : 0.9,
         "speed" : 0.4,
-        "combat skills" : 0.4,
+        "combat-skills" : 0.4,
         "image" : "https://www.spongebobshop.com/cdn/shop/products/SB-Standees-Spong-3_1200x.jpg?v=1603744568"
-},
+    },
 {
         "name" : "Senku",
         "power" : 0.9,
         "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1mdHjAqzPc5-gk1krr-dSBOxXr82sOzLcZw&s"
     },
     {
@@ -150,7 +189,7 @@ characterData = [
         "power" : 0.9,
         "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://statics.ruadoll.com/image/cache/2023/1103/0/27a9ad9d-3363-75a0-bd42-c88c90613ee9-1000x1000.jpg"
     },
     {
@@ -158,7 +197,7 @@ characterData = [
         "power" : 0.9,
         "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://static1.srcdn.com/wordpress/wp-content/uploads/2021/08/Ban-in-The-Seven-Deadly-Sins.jpg"
     },
     {
@@ -166,7 +205,7 @@ characterData = [
         "power" : 0.9,
         "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/64202805dcfea1001d0b7a1a.jpg"
     },
     {
@@ -174,31 +213,31 @@ characterData = [
         "power" : 0.9,
         "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://cdns-images.dzcdn.net/images/cover/30e780aee426c1a60d6c866452ce41f4/0x1900-000000-80-0-0.jpg"
     },
     {
         "name" : "Jiraiya",
         "power" : 0.9,
         "durability" : 0.6,
-        "speed" : 0.6,
-        "combat skills" : 0.9,
-        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxAcUrCIuZmvH0k6co6zeEL58T3uSt5qkFKA&s"
+        "speed" : 0.8,
+        "combat-skills" : 0.9,
+        "image" : "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/JiraiyaNarutomanga.jpg/150px-JiraiyaNarutomanga.jpg"
     },
     {
         "name" : "Inosuke",
         "power" : 0.9,
-        "durability" : 0.9,
+        "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
-        "image" : "https://thathashtagshow.com/wp-content/uploads/2022/01/demon-slayer-inosuke-rescue-season-2-cliffhanger-1024x536.jpg"
+        "combat-skills" : 0.9,
+        "image" : "https://m.media-amazon.com/images/I/71q7TSSqO6L._AC_UF894,1000_QL80_.jpg"
     },
     {
         "name" : "Jack Sparrow",
         "power" : 0.9,
         "durability" : 0.6,
         "speed" : 0.8,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPMMq7m9IwMxqFv4Z4JX3zh5WYgq8qmS9mqw&s"
 
     },
@@ -207,7 +246,7 @@ characterData = [
        "power" : 0.3,
        "durability" : 0.9,
        "speed" : 0.6,
-       "combat skills" : 0.5,
+       "combat-skills" : 0.5,
        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd1AQ68ks-pTURZ3Hhv-Pvk9uNB6iAFqN1Kw&s"
     },
 
@@ -216,8 +255,8 @@ characterData = [
         "power" : 0.8,
         "durability" : 0.4,
         "speed" : 0.5,
-        "combat skills" : 0.9,
-        "image" : "https://www.clipartmax.com/png/small/5-50354_doctor-julius-barber-dr-barber-flapjack.png"
+        "combat-skills" : 0.9,
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqBVE3URMIq7UAD-rW2WNcw4fAvp7kgWBZdw&s"
     },
     
     {
@@ -225,7 +264,7 @@ characterData = [
         "power" : 0.9,
         "durability" : 0.7,
         "speed" : 0.6,
-        "combat skills" : 0.9,
+        "combat-skills" : 0.9,
         "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR2F5Q47U9PZkAE0CvxLyUGyfFtvJlHJmGdQ&s"
     },
     
@@ -234,8 +273,8 @@ characterData = [
         "power" : 0.9,
         "durability" : 0.7,
         "speed" :  0.5,
-        "combat skills" :  0.9,
-        "image" : "https://static1.moviewebimages.com/wordpress/wp-content/uploads/2024/10/shrek.jpg"
+        "combat-skills" :  0.9,
+        "image" : "https://variety.com/wp-content/uploads/2022/05/MCDSHRE_EC008.jpg?w=1000&h=667&crop=1"
     },
     
     {
@@ -243,7 +282,7 @@ characterData = [
         "power" :  0.7,
         "durability" : 0.8,
         "speed" :  0.7,
-        "combat skills" :  0.8,
+        "combat-skills" :  0.8,
         "image" : "https://static.wixstatic.com/media/449abe_7b15686b2118476da4a733a7861dd772~mv2.jpg/v1/fill/w_1080,h_1080,al_c,q_85/sn-color.jpg"
     },
     
@@ -252,16 +291,16 @@ characterData = [
         "power" :  0.1,
         "durability" : 0.9,
         "speed" :  0.5,
-        "combat skills" : 0.5,
-        "image" : "https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Patrick_Star.svg/1200px-Patrick_Star.svg.png"   
- },
+        "combat-skills" : 0.5,
+        "image" : "https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Patrick_Star.svg/1200px-Patrick_Star.svg.png"
+    },
     
     {
         "name" : "Eren",
         "power" : 0.9,
         "durability" : 0.7,
         "speed" : 0.5,
-        "combat skills" :  0.9,
+        "combat-skills" :  0.9,
         "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkcKGGLKhsBCkkPFT7Cu6cZB1Q3XjxdZZsMA&s"
     },
     
@@ -270,7 +309,7 @@ characterData = [
         "power" : 0.2,
         "durability" : 0.6,
         "speed" : 0.5,
-        "combat skills" :  0.5,
+        "combat-skills" :  0.7,
         "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOL6pGmDI5beMercY5pjlPqJ18Q1RwmyHmhw&s"
     },
     
@@ -279,8 +318,8 @@ characterData = [
         "power" : 0.6,
         "durability" : 0.5,
         "speed" : 0.2,
-        "combat skills" :  0.9,
-        "image" : "https://www.clipartmax.com/png/small/467-4677422_dock-hag-flapjack-wiki-harbor-marvelous-misadventures-of-flapjack-dock-hag.png"
+        "combat-skills" :  0.9,
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmvjVB7Zwhz7YE-Buskmo4SY4pDfT3L3ezSg&s"
     },
 
     {
@@ -288,18 +327,27 @@ characterData = [
         "power" : 0.8,
         "durability" :  0.3,
         "speed" : 0.4,
-        "combat skills" :  0.9,
-        "image" : "https://qph.cf2.quoracdn.net/main-qimg-6299e66525633fddbfdb7e00f63d1e8e"
+        "combat-skills" :  0.9,
+        "image" : "https://wallpapersok.com/images/hd/neon-jotaro-kujo-and-star-platinum-r5liu43y5oyfkydk.jpg"
     },
     {
-        "name" : "Toby the Tram Engine",
-        "power" : 0.6,
-        "durability" :  0.5,
-        "speed" : 0.8,
-        "combat skills" :  0.1,
-        "image" : "https://upload.wikimedia.org/wikipedia/en/4/45/TobyCGIpromo.png"
+    "name" : "Yujiro Hanma",
+    "power" : 0.8,
+    "durability" : 0.8,
+    "speed" : 0.8,
+    "combat-skills" : 0.8,
+    "image" : "https://win.gg/_next/image/?url=https%3A%2F%2Fapi3.win.gg%2Fwp-content%2Fuploads%2F2023%2F08%2FYujiro-Hanma-Workout-1-1024x576-1.jpg&w=3840&q=75"
     },
-]
+    {
+    "name": "Sans",
+    "power" : 0.6,
+    "durability" : 0.1,
+    "speed" : 0.8,
+    "combat-skills" : 0.9,
+    "image" : "https://ih1.redbubble.net/image.434433009.6328/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u2.jpg"
+    },
+
+];
 let player1Traits = { "power": null, "durability": null, "speed": null, "combat skills": null };
 let player2Traits = { "power": null, "durability": null, "speed": null, "combat skills": null };
 
